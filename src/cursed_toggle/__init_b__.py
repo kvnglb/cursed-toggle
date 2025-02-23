dc082a9fce742d47a2b26ddceb4c01ab563e7321
kvnglb <151143873+kvnglb@users.noreply.github.com>
2025-02-23 14:11:51 +0100

    update

--- src/cursed_toggle/__init_b__.py
+++ src/cursed_toggle/__init_b__.py
@@ -2,16 +2,10 @@ import os
 import random
 
 testing = os.environ.get("SECRET_ENV_FOR_TESTING_ONLY_DO_NOT_USE")
+r = random.randint(1, 2)
 
-if testing == "1":
+if testing == "1" or (not testing and r == 1):
     from .cursed_toggle_v1_deprecated import cursed_toggle, _cursed_toggle
 
-elif testing == "2":
+elif testing == "2" or (not testing and r == 2):
     from .cursed_toggle_v2 import cursed_toggle, _cursed_toggle
-
-else:
-    if random.randint(1, 2) == 1:
-        from .cursed_toggle_v1_deprecated import cursed_toggle, _cursed_toggle
-
-    else:
-        from .cursed_toggle_v2 import cursed_toggle, _cursed_toggle
