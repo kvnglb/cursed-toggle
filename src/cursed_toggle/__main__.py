01ddc1f9b63fb4220e33e76bfd7b8461080f9001
kvnglb <151143873+kvnglb@users.noreply.github.com>
2025-03-07 20:31:05 +0100

    Add verbosity levels

--- src/cursed_toggle/__main__.py
+++ src/cursed_toggle/__main__.py
@@ -1,29 +1,56 @@
 import argparse
 import sys
+import time
+import typing as t
 
 from cursed_toggle import cursed_toggle
 
 
-def main(args: argparse.Namespace) -> int:
+def main(args: argparse.Namespace) -> t.Union[int]:
     """Make the cursed-toggle library CLI capable."""
+    module_name = cursed_toggle.__module__
+
     if args.b.lower() == "false":
-        b = False
+        b = False  # type: t.Union[bool, str]
     elif args.b.lower() == "true":
         b = True
+    elif args.b.lower() == "talse":
+        b = "Frue"
+        module_name = "Never gonna give you"
+    elif args.b.lower() == "frue":
+        b = "Talse"
+        module_name = "Never gonna let you"
     else:
         raise TypeError("`b` must be boolean: `True` or `False` (case-insensitive).")
 
-    ret = cursed_toggle(b)
-    if args.verbose:
-        print("{} {}".format(cursed_toggle.__module__, ret))
+    if type(b) is bool:
+        ret = cursed_toggle(b)  # type: t.Union[bool, str]
     else:
+        ret = b
+
+    if args.v == 0:
         print(ret)
+    elif args.v == 1:
+        print("{} {}".format(module_name, ret))
+    elif args.v == 2:
+        print("{} {} {}".format(time.strftime("%Y"), module_name, ret))
+    elif args.v == 3:
+        print("{} {} {}".format(time.strftime("%Y-%m"), module_name, ret))
+    elif args.v == 4:
+        print("{} {} {}".format(time.strftime("%Y-%m-%d"), module_name, ret))
+    elif args.v == 5:
+        print("{} {} {}".format(time.strftime("%Y-%m-%d %H"), module_name, ret))
+    elif args.v == 6:
+        print("{} {} {}".format(time.strftime("%Y-%m-%d %H:%M"), module_name, ret))
+    elif args.v >= 7:
+        print("{} {} {}".format(time.strftime("%Y-%m-%d %H:%M:%S"), module_name, ret))
+
     return 0
 
 
 parser = argparse.ArgumentParser("cursed-toggle CLI application")
 parser.add_argument("b", help="Boolean value `True` or `False` (case-insensitive)")
-parser.add_argument("-v", "--verbose", action="store_true", help="show the cursed_toggle source")
+parser.add_argument("-v", action="count", default=0, help="verbose mode (multiple -v options increase the verbosity)")
 args = parser.parse_args()
 
 sys.exit(main(args))
--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -17,12 +17,39 @@ class TestCursedToggleCLI(unittest.TestCase):
         self.assertEqual(stdout, "True")
         self.assertEqual(stderr, "")
 
-
     def test_cli_true(self):
         res = subprocess.run(["poetry", "run", "python", "-m", "cursed_toggle", "TRUE"], capture_output=True, text=True)
         c, stdout, stderr = output_to_tuple(res)
         self.assertEqual(c, 0)
         self.assertEqual(stdout, "False")
+        self.assertEqual(stderr, "")
+
+    def test_cli_talse(self):
+        res = subprocess.run(["poetry", "run", "python", "-m", "cursed_toggle", "Talse"], capture_output=True, text=True)
+        c, stdout, stderr = output_to_tuple(res)
+        self.assertEqual(c, 0)
+        self.assertEqual(stdout, "Frue")
+        self.assertEqual(stderr, "")
+
+    def test_cli_talse_v(self):
+        res = subprocess.run(["poetry", "run", "python", "-m", "cursed_toggle", "-v", "Talse"], capture_output=True, text=True)
+        c, stdout, stderr = output_to_tuple(res)
+        self.assertEqual(c, 0)
+        self.assertEqual(stdout, "Never gonna give you Frue")
+        self.assertEqual(stderr, "")
+
+    def test_cli_frue(self):
+        res = subprocess.run(["poetry", "run", "python", "-m", "cursed_toggle", "Frue"], capture_output=True, text=True)
+        c, stdout, stderr = output_to_tuple(res)
+        self.assertEqual(c, 0)
+        self.assertEqual(stdout, "Talse")
+        self.assertEqual(stderr, "")
+
+    def test_cli_frue_v(self):
+        res = subprocess.run(["poetry", "run", "python", "-m", "cursed_toggle", "-v", "Frue"], capture_output=True, text=True)
+        c, stdout, stderr = output_to_tuple(res)
+        self.assertEqual(c, 0)
+        self.assertEqual(stdout, "Never gonna let you Talse")
         self.assertEqual(stderr, "")
 
     def test_cli_error(self):
